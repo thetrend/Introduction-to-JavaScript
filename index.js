e4aa77d44c1ace8a23b05dc73cc82afbcb7bd08e
Grace de la Mora <grace.g.delamora@gmail.com>
2021-02-27 00:00:21 -0500

    Task 2 completed

--- index.js
+++ index.js
@@ -82,11 +82,12 @@ Do the following:
    3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
+function dogYears(age){
     /*add your code here*/
+    return age * 7;
 }
 
-
+console.log(dogYears(15));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
